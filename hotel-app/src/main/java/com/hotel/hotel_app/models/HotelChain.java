93a5b392b56b8453809c9b8d25b49318c50d4cd1
Olivier Currie <olivier_currie@icloud.com>
2026-04-07 16:33:23 -0400

    more

--- hotel-app/src/main/java/com/hotel/hotel_app/models/HotelChain.java
+++ hotel-app/src/main/java/com/hotel/hotel_app/models/HotelChain.java
@@ -8,9 +8,9 @@ public class HotelChain {
 
     private String name;
 
-    private String email;
+    //private String email;
 
-    private String phone_number;
+    //private String phone_number;
 
     private long num_hotels;
 
@@ -18,15 +18,15 @@ public class HotelChain {
 
     public HotelChain(String name, String email, String phone_number, long num_hotels, String address) {
         this.name = name;
-        this.email = email;
-        this.phone_number = phone_number;
+        //this.email = email;
+        //this.phone_number = phone_number;
         this.num_hotels = num_hotels;
         this.address = address;
     }
 
     public String getName() {return this.name;}
-    public String getEmail() {return this.email;}
-    public String getPhone() {return this.phone_number;}
+    //public String getEmail() {return this.email;}
+    //public String getPhone() {return this.phone_number;}
     public Long getNumHotels() {return this.num_hotels;}
     public String getAddress() {return this.address;}
 }
