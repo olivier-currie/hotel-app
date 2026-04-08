37ced41b238c4e3a506f61945b69475ace75b5fa
Victor <victorgao27@gmail.com>
2026-04-07 23:46:05 -0400

    Added service files

---
+++ hotel-app/src/main/java/com/hotel/hotel_app/services/BookingService.java
@@ -0,0 +1,18 @@
+package com.hotel.hotel_app.services;
+
+import com.hotel.hotel_app.models.Booking;
+import com.hotel.hotel_app.repository.BookingRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookingService {
+    private final BookingRepository bookingR;
+
+    public BookingService(BookingRepository bookingR) {
+        this.bookingR = bookingR;
+    }
+
+    public Booking createBooking(Booking booking) {
+        return bookingR.save(booking);
+    }
+}
