5ada780ca8bdb14c2f2872ed4ec4e66d6c33c15b
Olivier Currie <olivier_currie@icloud.com>
2026-04-08 19:40:19 -0400

    added some stuff

--- hotel-app/src/main/java/com/hotel/hotel_app/controllers/BookingController.java
+++ hotel-app/src/main/java/com/hotel/hotel_app/controllers/BookingController.java
@@ -1,10 +1,23 @@
 package com.hotel.hotel_app.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hotel.hotel_app.models.Booking;
+import com.hotel.hotel_app.services.BookingService;
 
 @RestController
 @RequestMapping("/api/bookings")
 public class BookingController {
-    
+    private final BookingService bookingService;
+
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    @PostMapping
+    public Booking createBooking(Booking booking) {
+        return bookingService.createBooking(booking);
+    }
 }
