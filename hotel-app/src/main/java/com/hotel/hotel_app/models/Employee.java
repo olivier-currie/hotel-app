f60f8e0804afeeb2a5a19b0a940ea91e436ff586
Olivier Currie <olivier_currie@icloud.com>
2026-04-07 20:12:47 -0400

    even more modeling

--- hotel-app/src/main/java/com/hotel/hotel_app/models/Employee.java
+++ hotel-app/src/main/java/com/hotel/hotel_app/models/Employee.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Table(name = "employee")
 public class Employee {
     @Id
-    private String customer_id;
+    private String employee_id;
 
     private String id_type;
 
@@ -17,21 +17,21 @@ public class Employee {
     private String address;
 
 
-    public Employee(String customer_id, String id_type, Long hotel_id, String name, String address) {
-        this.customer_id = customer_id;
+    public Employee(String employee_id, String id_type, Long hotel_id, String name, String address) {
+        this.employee_id = employee_id;
         this.id_type = id_type;
         this.hotel_id = hotel_id;
         this.name = name;
         this.address = address;
     }
 
-    public String getCustomerID() {return this.customer_id;}
+    public String getCustomerID() {return this.employee_id;}
     public String getIDType() {return this.id_type;}
     public Long getHotelID() {return this.hotel_id;}
     public String getName() {return this.name;}
     public String getAddress() {return this.address;}
 
-    public void setCustomerID(String customer_id) {this.customer_id = customer_id;}
+    public void setCustomerID(String employee_id) {this.employee_id = employee_id;}
     public void setIDType(String id_type) {this.id_type = id_type;}
     public void setHotelID(Long hotel_id) {this.hotel_id = hotel_id;}
     public void setName(String name) {this.name = name;}
