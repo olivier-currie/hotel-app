e537eb6ab327bd768fd5c50216797411f61a3530
Olivier Currie <olivier_currie@icloud.com>
2026-04-07 23:16:24 -0400

    fixed typos

--- hotel-app/src/main/java/com/hotel/hotel_app/repositories/BookingRepository.java
+++ hotel-app/src/main/java/com/hotel/hotel_app/repositories/BookingRepository.java
@@ -1,10 +1,10 @@
 package com.hotel.hotel_app.repositories;
 
 import com.hotel.hotel_app.models.Booking;
-import org.springframework.data.jpa.respository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookingRespository extends JpaRepository<Booking, Long> {
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 
 }
--- hotel-app/src/main/java/com/hotel/hotel_app/repositories/RentingRepository.java
+++ hotel-app/src/main/java/com/hotel/hotel_app/repositories/RentingRepository.java
@@ -1,10 +1,10 @@
 package com.hotel.hotel_app.repositories;
 
 import com.hotel.hotel_app.models.Renting;
-import org.springframework.data.jpa.respository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RentingRespository extends JpaRepository<Renting, Long> {
+public interface RentingRepository extends JpaRepository<Renting, Long> {
 
 }
