a27245cdd2f3d48c19d6fd247f40f16c41d8e828
Olivier Currie <olivier_currie@icloud.com>
2026-04-08 21:50:43 -0400

    fixing mistakes

--- hotel-app/src/main/java/com/hotel/hotel_app/controllers/RentingController.java
+++ hotel-app/src/main/java/com/hotel/hotel_app/controllers/RentingController.java
@@ -1,10 +1,15 @@
-package com.hotel.hotel_app.services;
+package com.hotel.hotel_app.controllers;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.hotel.hotel_app.models.Renting;
+import com.hotel.hotel_app.services.RentingService;
 
+@RestController
+@RequestMapping("/api/rooms")
 public class RentingController {
     private final RentingService rentingService;
 
